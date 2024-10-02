75947d826b86d1d93c6f2304e76cff32085b06e6
musho02 <natpakan2@gmail.com>
2024-10-02 23:46:10 +0700

    cors update

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const fs = require('fs');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 const upload = multer({
   storage: multer.memoryStorage(),
