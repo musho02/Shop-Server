0fb628dfa02c2eb69b2178edef424cc9c2217e36
musho02 <natpakan2@gmail.com>
2024-10-02 23:59:35 +0700

    cors update

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const upload = multer({
 });
 
 app.use(cors({
-  origin: 'https://shop-react-jbg3.onrender.com', // เปลี่ยนเป็นโดเมนของ frontend
+  origin: '*', // เปลี่ยนเป็นโดเมนของ frontend
   methods: 'GET,POST', // หรือระบุวิธีที่คุณใช้
   credentials: true // หากจำเป็น
 }));
